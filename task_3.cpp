5e704729262358d0efb922432a3063d0490ef5da
VanyaSO <ushachovg324@gmail.com>
2024-03-04 09:03:41 +0200

    add fix

--- task_3.cpp
+++ task_3.cpp
@@ -81,5 +81,8 @@ int main()
               negativeArr,negativeSize,
                      zeroArr, zeroSize);
 
+    delete[] positiveArr;
+    delete[] negativeArr;
+    delete[] zeroArr;
     return 0;
 }
